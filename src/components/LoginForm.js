eb36a5524b439bafad857204d8ee2e56dade49e0
jasonbasuil <basuilj@gmail.com>
2019-04-02 17:05:20 -0700

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,24 +2,50 @@ import React from "react";
 
 class LoginForm extends React.Component {
   constructor() {
-    super();
 
-    this.state = {};
+    super();
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  handleInputChange = ev => {
+    this.setState({
+    [ev.target.name]: ev.target.value
+    })
+  }
+
+  handleSubmit = ev => {
+    ev.preventDefault()
+    if (!this.state.username || !this.state.password) return
+    this.props.onSubmit(this.state)
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={ev => this.handleSubmit(ev)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input
+              id="username"
+              name="username"
+              type="text"
+              value={this.state.username}
+              onChange={ev => this.handleInputChange(ev)}
+            />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input
+              id="password"
+              name="password"
+              type="password"
+              onChange={ev => this.handleInputChange(ev)}
+            />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,25 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      message: ''
+    };
+  }
+
+  handleKeyPress = (ev) => {
+    this.setState({
+      message: ev.target.value})
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" onChange={ev => this.handleKeyPress(ev)} value={this.state.message}/>
+        <p>{this.props.maxChars - this.state.message.length}</p>
       </div>
     );
   }
